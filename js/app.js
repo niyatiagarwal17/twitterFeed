d3ee44cf2b72dbe5ac985dd87ee676c5e2e32490
niyatiagarwal17 <niyatiagarwal17@gmail.com>
2016-08-22 17:27:43 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,45 @@
+angular.module('twitterFeed', [])
+  .controller('MainCtrl', ['$location',function($location) {
+    var self = this;
+    self.tab = 'tweets';
+    self.open = function(tab) {
+      self.tab = tab;
+    };
+	 self.isActive = function (viewLocation) { 
+        return viewLocation === $location.path();
+    };
+  }])
+  .controller('TweetsCtrl', [function() {
+    var self = this;
+    self.list = [
+      {name: 'Charles Carney', tweet: 'Sed egestas, ante et vulputate volutpat, eros pede semper est, vitae luctus metus libero eu augue. Morbi purus libero, faucibus adipiscing, commodo quis, gravida id, est. Sed lectus. Praesent elementum hendrerit tortor. Sed semper lorem at felis. Vestibulum volutpat, lacus a ultrices sagittis, mi neque euismod dui, eu pulvinar nunc sapien ornare nisl. Phasellus pede arcu, dapibus eu, fermentum et, dapibus sed, urna.',url:'@carney',img:''},
+      {name: 'Michael Solwecki', tweet: 'Morbi interdum mollis sapien. Sed ac risus. Phasellus lacinia, magna a ullamcorper laoreet, lectus arcu pulvinar risus, vitae facilisis libero dolor a purus. Sed vel lacus. Mauris nibh felis, adipiscing varius, adipiscing in, lacinia vel, tellus. Suspendisse ac urna. Etiam pellentesque mauris ut lectus. Nunc tellus ante, mattis eget, gravida vitae, ultricies ac, leo. Integer leo pede, ornare a, lacinia eu, vulputate vel, nisl.',url:'@solwecki',img:''},
+	  {name: 'Christopher Craig', tweet: 'Sed ac risus. Phasellus lacinia, magna a ullamcorper laoreet, lectus arcu pulvinar risus, vitae facilisis libero dolor a purus. Sed vel lacus. Mauris nibh felis, adipiscing varius, adipiscing in, lacinia vel, tellus. Suspendisse ac urna. Etiam pellentesque mauris ut lectus. Nunc tellus ante, mattis eget, gravida vitae, ultricies ac, leo. Integer leo pede, ornare a, lacinia eu, vulputate vel, nisl.',url:'@craig',img:'img/vido-img.jpg'}
+    ];
+
+	self.checkImg = function(src){
+		if(src=="" || src==null)
+			return false;
+		else 
+			return true;
+	};
+  }])
+    .controller('PhotosCtrl', [function() {
+    var self = this;
+    self.list = [
+	{text:'Sed egestas, ante et vulputate volutpat, eros pede semper est, vitae luctus metus libero eu augue. Morbi purus libero, faucibus adipiscing, commodo quis, gravida id, est. Sed lectus. Praesent elementum hendrerit tortor. Sed semper lorem at felis. Vestibulum volutpat, lacus a ultrices sagittis, mi neque euismod dui, eu pulvinar nunc sapien ornare nisl. Phasellus pede arcu, dapibus eu, fermentum et, dapibus sed, urna.'}
+    ];
+  }])
+  .controller('FollowingCtrl', [function() {
+   var self = this;
+    self.list = [
+	{text:'Morbi interdum mollis sapien. Sed ac risus. Phasellus lacinia, magna a ullamcorper laoreet, lectus arcu pulvinar risus, vitae facilisis libero dolor a purus. Sed vel lacus. Mauris nibh felis, adipiscing varius, adipiscing in, lacinia vel, tellus. Suspendisse ac urna. Etiam pellentesque mauris ut lectus. Nunc tellus ante, mattis eget, gravida vitae, ultricies ac, leo. Integer leo pede, ornare a, lacinia eu, vulputate vel, nisl.'}
+    ];
+  }])
+  .controller('FollowersCtrl', [function() {
+    var self = this;
+    self.list = [
+	{text:'Sed ac risus. Phasellus lacinia, magna a ullamcorper laoreet, lectus arcu pulvinar risus, vitae facilisis libero dolor a purus. Sed vel lacus. Mauris nibh felis, adipiscing varius, adipiscing in, lacinia vel, tellus. Suspendisse ac urna. Etiam pellentesque mauris ut lectus. Nunc tellus ante, mattis eget, gravida vitae, ultricies ac, leo. Integer leo pede, ornare a, lacinia eu, vulputate vel, nisl.'}
+    ];
+  }]);
+  
